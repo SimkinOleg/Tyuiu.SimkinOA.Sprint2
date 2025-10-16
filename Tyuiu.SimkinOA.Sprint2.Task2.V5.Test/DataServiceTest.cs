11f44b96a0730d927bd7ee68ac5a755bb937a769
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-10-16 20:03:20 +0500

    Task2

---
+++ Tyuiu.SimkinOA.Sprint2.Task2.V5.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.SimkinOA.Sprint2.Task2.V5.Lib;
+namespace Tyuiu.SimkinOA.Sprint2.Task2.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void GeLogicOperations()
+        {
+            DataService ds = new DataService();
+            int x = 6;
+            int y = 7;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SimkinOA.Sprint2.Task2.V5/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.SimkinOA.Sprint2.Task2.V5.Lib;
+namespace Tyuiu.SimkinOA.Sprint2.Task2.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y: ");
+            int y = Convert.ToInt32(Console.ReadLine());    
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            if (res)
+            {
+                Console.WriteLine("Точка находится в заданной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится в заданной области");
+            }
+        }
+    }
+}

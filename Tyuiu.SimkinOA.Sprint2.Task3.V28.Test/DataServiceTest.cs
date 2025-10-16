f59aef54bd69e4b5ae0ce5d14f6b69d23332bc7a
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-10-16 20:17:48 +0500

    Task3

---
+++ Tyuiu.SimkinOA.Sprint2.Task3.V28.Test/DataServiceTest.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.SimkinOA.Sprint2.Task3.V28.Lib;
+namespace Tyuiu.SimkinOA.Sprint2.Task3.V28.Test
+{
+    
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 1.881;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = -5;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -2;
+            double res = ds.Calculate(x);
+            double wait = 0.034;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -33;
+            double res = ds.Calculate(x);
+            double wait = -362.788;
+            Assert.AreEqual(wait, res);
+        }
+    }
+
+
+
+}

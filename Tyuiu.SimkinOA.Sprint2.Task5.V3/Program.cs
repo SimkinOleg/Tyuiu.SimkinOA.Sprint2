45b0d66c427964d31956d2bf7bdeb12c7b31c6e3
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-10-16 21:42:47 +0500

    Task5

---
+++ Tyuiu.SimkinOA.Sprint2.Task5.V3/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.SimkinOA.Sprint2.Task5.V3.Lib;
+namespace Tyuiu.SimkinOA.Sprint2.Task5.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер дня недели:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+
+            string res;
+            if ((num < 1) || (num > 7))
+            {
+                res = "Введено неверное значение!";
+            }
+            else
+            {
+                res = "Это день недели: " + ds.FindDayName(num);
+            }
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+}
